281c8acda21eb6428165b4abc5a74d93ee21e293
mihaamiharu <@ekkisyam23@gmail.com>
2021-07-31 11:45:08 +0700

    Spec for when no one like post

---
+++ Module_4_TDD/Session_1/app/wli.rb
@@ -0,0 +1,12 @@
+class WhoLikesIt
+
+    def initialize
+        @name = []
+    end
+
+    def likes
+        @name.length == 0
+        return "no one like this"
+    end
+    
+end
---
+++ Module_4_TDD/Session_1/spec/wli_spec.rb
@@ -0,0 +1,7 @@
+require '../app/wli'
+
+describe WhoLikesIt do
+    it "no one like this" do
+        wli = WhoLikesIt.new
+    end
+end
