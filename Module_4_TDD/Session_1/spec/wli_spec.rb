6a9ccfc8b38f05e6e7fa59591881b3fe91257367
mihaamiharu <@ekkisyam23@gmail.com>
2021-07-31 11:54:48 +0700

    Give spec when likes is 0 or 1

--- Module_4_TDD/Session_1/spec/wli_spec.rb
+++ Module_4_TDD/Session_1/spec/wli_spec.rb
@@ -3,5 +3,17 @@ require '../app/wli'
 describe WhoLikesIt do
     it "no one like this" do
         wli = WhoLikesIt.new
+        wli.names = []
+        p wli.likes #p stands for puts
+        expect(wli.likes).to eq("no one like this")
     end
+
+    it "one person likes this" do
+        wli = WhoLikesIt.new
+        wli.names = ["Tata"]
+        p wli.likes #p stands for puts
+        expect(wli.likes).to eq("Tata likes this")
+    end
+
+
 end
